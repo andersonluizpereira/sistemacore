32f1559b7181f9a0dcb0120871e8de4ad9534949
acpereira <Anderson.Pereira-ext@fnac.com.br>
2017-05-09 16:36:02 -0300

    core

---
+++ Sistema.Domain.Blog/AccountViewModels/VerifyCodeViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Sistema.Domain.Entities.AccountViewModels
+{
+   public class VerifyCodeViewModel
+    {
+        [Required]
+        public string Provider { get; set; }
+
+        [Required]
+        public string Code { get; set; }
+
+        public string ReturnUrl { get; set; }
+
+        [Display(Name = "Remember this browser?")]
+        public bool RememberBrowser { get; set; }
+
+        [Display(Name = "Remember me?")]
+        public bool RememberMe { get; set; }
+    }
+}
